7f49098965c1d8a2533e953721bcc63bf3e4ddfa
Fenikkusu <5033618+Fenikkusu@users.noreply.github.com>
2021-07-13 21:32:48 -0400

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,11 +4,11 @@
 : "${KOMGA_APP_DIR:=/usr/local/komga}"
 
 # Install komga
-pw useradd komga -u 5469 -s /bin/csh -m
+pw useradd komga -u 8675309 -s /bin/csh -m
 komga-update || { echo "❌ Komga install failed." >&2 ; exit 1; }
 chown -R komga $KOMGA_APP_DIR
 
-cp /usr/local/etc/komga-plugin.conf.sample /usr/local/etc/komga-plugin.conf
+cp /usr/local/etc/komga-plugin.conf.sample /usr/local/etc/komga-plugin.conf || true
 sysrc -f /etc/rc.conf komga_enable="YES"
 
 service komga start
